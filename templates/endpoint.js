3397a751318e0e4efd633f64c1972fd3340a20bd
Johan Griesel <johan@stratusolve.com>
2021-09-22 11:29:56 +0200

    Working on properly defining starting points for packages

--- templates/endpoint.js
+++ templates/endpoint.js
@@ -1,1 +1,30 @@
 const dx = require('[dxAppScriptRequire]');
+const [packageName]Controller = require('./index');
+const divbloxEndpointBase = require('divbloxjs/dx-core-modules/endpoint-base');
+
+class [packageName]Endpoint extends divbloxEndpointBase {
+    constructor() {
+        super();
+        // TODO: Declare any additional operations here
+        this.addOperations(["getPackageName"]);
+    }
+
+    async executeOperation(operation, request) {
+        await super.executeOperation(operation, request);
+
+        switch(operation) {
+            case 'getPackageName': await this.test();
+                break;
+            // TODO: Add additional cases here for each declared operation
+        }
+    }
+
+    async getPackageName() {
+        this.setResult(true, "Package name is [packageName]");
+    }
+
+    // TODO: Add implementations for each declared operation below
+}
+const [packageName]EndpointInstance = new [packageName]Endpoint();
+
+module.exports = [packageName]EndpointInstance;
--- templates/index.js
+++ templates/index.js
@@ -1,1 +1,13 @@
 const dx = require('[dxAppScriptRequire]');
+const divbloxPackageControllerBase = require('divbloxjs/dx-core-modules/package-controller-base');
+
+class [packageName] extends divbloxPackageControllerBase {
+    constructor() {
+        super();
+        // TODO: Override as required
+    }
+    // TODO: Add package specific functionality below
+}
+const [packageName]Instance = new [packageName]();
+
+module.exports = [packageName]Instance;
