aa76903b0b178418c682194253a801017728689a
Johan Griesel <johan@stratusolve.com>
2021-11-09 10:39:28 +0200

    Update endpoint.js

--- templates/endpoint.js
+++ templates/endpoint.js
@@ -5,17 +5,30 @@ const divbloxEndpointBase = require('divbloxjs/dx-core-modules/endpoint-base');
 class [packageNamePascalCase]Endpoint extends divbloxEndpointBase {
     constructor() {
         super();
-        this.endpointName = "[packageName]";
-        this.endpointDescription = "[packageName] endpoint";
+
+        this.endpointName = "[packageName]"; // Change this to set the actual url endpoint
+        this.endpointDescription = "[packageName] endpoint"; // Change this to be more descriptive of the endpoint
 
         // TODO: Declare any additional operations here
         const getPackageName = this.getOperationDefinition(
             {
                 "operationName": "getPackageName",
-                "allowedAccess": ["anonymous"]
+                "allowedAccess": ["anonymous"], // If this array does not contain "anonymous", a JWT token will be expected in the Auth header
+                "operationDescription": "This sentence describes the operation",
+                "parameters": [], // An array of this.getInputParameter()
+                "requestType": "GET", // GET|POST|PUT|DELETE|OPTIONS|HEAD|PATCH|TRACE
+                "requestSchema": {}, // this.getSchema()
+                "responseSchema": {}, // this.getSchema()
             }
         );
+
+        // You need to do this in order for the operation to be available on the endpoint.
+        // Also, this declaration provides the necessary input for swagger ui present the docs for this
         this.declareOperations([getPackageName]);
+
+        // TODO: Declare any entity schemas here if needed
+        // An example of how to declare entity schemas for swagger ui
+        //this.declareEntitySchemas(["anEntityInYourDataModel"]);
     }
 
     async executeOperation(operation, request, dxInstance = null) {
