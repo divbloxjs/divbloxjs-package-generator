dad1841b12b0bfdea15594eb34d2760301888cf2
git <johan@stratusolve.com>
2022-09-05 12:33:20 +0200

    Minor fix

--- templates/index.js
+++ templates/index.js
@@ -6,12 +6,12 @@ const DivbloxBase = require("divbloxjs/divblox");
 */
 
 class [packageNamePascalCase] extends divbloxPackageControllerBase {
+    /**
+     * A basic constructor that can be overridden
+     * @param {DivbloxBase} dxInstance An instance of divbloxjs to allow for access to the app configuration
+     * @param {string} packageName The name given to this package
+     */
     constructor(dxInstance = null, packageName = '[packageNameDefault]') {
-        /**
-         * A basic constructor that can be overridden
-         * @param {DivbloxBase} dxInstance An instance of divbloxjs to allow for access to the app configuration
-         * @param {string} packageName The name given to this package
-         */
         super(dxInstance, packageName);
         // TODO: Override as required
     }
